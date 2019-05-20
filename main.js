528c36798db5e40bb9449df868494962895eb6ec
abhi <abrahmbhatt03@gmai.com>
2019-05-20 16:30:54 -0500

    Done with js code for porject

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const host = 'https://api.winnipegtransit.com/v3/';
 const apiKey = 'api-key=6ijhpcl278IX2xWSPhyS';
-const query = 'streets/1717.json?'
+const query = 'streets.json?name=henlow bay&';
+const stops = 'stops.json?street='
+let stopsArray;
+let promises = [];
+
+const stopSchedules = stopKey => {
+  return fetch(`${host}stops/${stopKey}/schedule.json?${apiKey}`)
+}
+
+const promise = json => {
+  json.stops.forEach(e => {
+    promises.push(stopSchedules(e.key))
+  })
+  return promises;
+}
+
+const addDataToHtml = jsonData => {
+  Promise.all(promise(jsonData))
+    .then((response) => {
+      stopsArray = response.map(e => e.json())
+    })
+
+
+}
+
 fetch(`${host}${query}${apiKey}`)
-  .then((response) => console.log(response.json()));
+  .then((response) => response.json())
+  .then((responseData) => fetch(`${host}${stops}${responseData.streets[0].key}&${apiKey}`))
+  .then((stopsRespose) => stopsRespose.json())
+  .then((stopsJson) => addData(stopsJson))
